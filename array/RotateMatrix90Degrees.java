eefb735de7b901edad6f6520356fb2249e186c08
sw4tiarora <aroraswxti@gmail.com>
2026-03-31 21:23:47 +0530

    Implement 90-degree matrix rotation using transpose and two-pointer row reversal in Java

---
+++ array/RotateMatrix90Degrees.java
@@ -0,0 +1,50 @@
+public class RotateMatrix90Degrees {
+    public static void rotateMatrix(int[][] matrix){
+        int n = matrix.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n-1;
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] mat) {
+        for (int[] row : mat) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotateMatrix(matrix);
+
+        System.out.println("\nRotated Matrix (90° clockwise):");
+        printMatrix(matrix);
+    }
+}
