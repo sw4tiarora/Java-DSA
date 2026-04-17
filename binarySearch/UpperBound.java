d784a004a68bf26617d20e56aad06760515fcd21
sw4tiarora <aroraswxti@gmail.com>
2026-04-17 19:45:04 +0530

    Add upper bound implementation using binary search in Java

---
+++ binarySearch/UpperBound.java
@@ -0,0 +1,33 @@
+public class UpperBound {
+    public static int upperBound(int[] arr, int n, int x){
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 3, 5, 7};
+        int x = 3;
+
+        int index = upperBound(arr, arr.length, x);
+        System.out.println("Upper bound index: " + index);
+
+        if(index < arr.length) {
+            System.out.println("Element at upper bound: " + arr[index]);
+        } else {
+            System.out.println("No element greater than " + x);
+        }
+    }
+}
