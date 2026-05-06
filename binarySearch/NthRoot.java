334dda8fa40082097420a8d45d2b364930e60a1a
sw4tiarora <aroraswxti@gmail.com>
2026-05-06 16:35:03 +0530

    Implemented binary search solution to find nth root of a number

---
+++ binarySearch/NthRoot.java
@@ -0,0 +1,35 @@
+public class NthRoot {
+    public static int func(int mid, int n, int m){
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if(ans > m) return 2;
+        }
+        if(ans == m) return 1;
+        return 0;
+    }
+
+    public static int nthRoot(int n, int m){
+        int low = 1, high = m;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if(midN == 1){
+                return mid;
+            }
+            else if(midN == 0){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 27;
+        System.out.println("Nth root is: " + nthRoot(n, m));
+    }
+}
