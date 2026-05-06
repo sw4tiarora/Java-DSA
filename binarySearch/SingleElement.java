feb3b1458707d58b4e84a6d679950f6128384e89
sw4tiarora <aroraswxti@gmail.com>
2026-05-06 12:10:36 +0530

    Implemented optimal binary search solution for single element in sorted array

--- binarySearch/SingleElement.java
+++ binarySearch/SingleElement.java
@@ -1,3 +1,34 @@
 public class SingleElement {
-    
+    public static int findSingle(int[] arr){
+        int n = arr.length;
+
+        //edge cases
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-2]) return arr[n-1];
+
+        int low = 1, high = n - 2;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]){
+                return arr[mid];
+            }
+
+            if((mid % 2 == 1 && arr[mid] == arr[mid-1]) || (mid % 2 == 0 && arr[mid] == arr[mid+1])){
+                low = mid + 1;
+            }
+
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,3,3,4,5,5,6,6};
+        int single = findSingle(arr);
+        System.out.println("The single element is: "+ single);
+    }
 }
